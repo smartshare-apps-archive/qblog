626ee94b28de1ba0aceaf2b6ea9f3d01f78ba648
smartshare-apps <smartshare.apps@gmail.com>
2017-10-09 16:40:47 -0400

    Revert "Google analytics dashboard"

--- application.py
+++ application.py
@@ -26,11 +26,10 @@ application = Flask(__name__)
 #generate a random secret key
 application.secret_key = os.urandom(24)
 
-application.config['ctl'] = BlogCMS()       #this instance of ControlPanel can be accessed through the application context so blueprints can use it
+application.config['ctl'] = BlogCMS()		#this instance of ControlPanel can be accessed through the application context so blueprints can use it
 application.config['session_cookie_id'] = "sess_id"
 application.config['SessionManager'] = session_manager.SessionManager()
-application.config['GOOGLE_API_KEY'] = 'AIzaSyDq21enWEblUp7Tvo7W4BNSr18p9BSRPnA'
-application.config['GOOGLE_API_JSON'] = 'client_secrets.json'
+
 assert application.config['SessionManager'].r is not None, "Can't connect to redis server."
 
 
@@ -42,41 +41,43 @@ application.register_blueprint(login.login_routes)
 
 #only for debugging, makes sure to check all the js files and templates for changes
 def debug_dirUpdate():
-    extra_dirs = ['templates/','static/js/']
-    extra_files = extra_dirs[:]
+	extra_dirs = ['templates/','static/js/']
+	extra_files = extra_dirs[:]
 
-    for extra_dir in extra_dirs:
-        for dirname, dirs, files in os.walk(extra_dir):
-            for filename in files:
-                filename = os.path.join(dirname, filename)
-                if os.path.isfile(filename):
-                    extra_files.append(filename)
+	for extra_dir in extra_dirs:
+	    for dirname, dirs, files in os.walk(extra_dir):
+	        for filename in files:
+	            filename = os.path.join(dirname, filename)
+	            if os.path.isfile(filename):
+	                extra_files.append(filename)
 
-    return extra_files
+	return extra_files
 
 
 
 @application.route('/sitemap.xml')
 def sitemap():
-    """Generate sitemap.xml """
-    pages = ["/index", "/", "/login", "/logout"]
-    # All pages registed with flask apps
-    db_wrapper = database_wrapper()
-    post_pages = ["/post/" + post_id for post_id, post_data in query.getAllPosts(db_wrapper.content).iteritems()]
-    post_pages += ["/post/" + post_data["post_handle"] for post_id, post_data in query.getAllPosts(db_wrapper.content).iteritems() if post_data["post_handle"]]
-    pages += post_pages
+	"""Generate sitemap.xml """
+	pages = ["/index", "/", "/login", "/logout"]
+	# All pages registed with flask apps
+	db_wrapper = database_wrapper()
+	post_pages = ["/post/" + post_id for post_id, post_data in query.getAllPosts(db_wrapper.content).iteritems()]
+	post_pages += ["/post/" + post_data["post_handle"] for post_id, post_data in query.getAllPosts(db_wrapper.content).iteritems() if post_data["post_handle"]]
+	pages += post_pages
 
-    sitemap_xml = render_template('sitemap_template.xml', pages=pages)
-    response = make_response(sitemap_xml)
-    response.headers["Content-Type"] = "application/xml"
+	sitemap_xml = render_template('sitemap_template.xml', pages=pages)
+	response = make_response(sitemap_xml)
+	response.headers["Content-Type"] = "application/xml"
 
-    return response
+	return response
+
+
 
 
 if __name__ == "__main__":
-    extra_files = debug_dirUpdate()
+	extra_files = debug_dirUpdate()
 
-    #live_host = "0.0.0.0"
-    live_host = "127.0.0.1"
+	#live_host = "0.0.0.0"
+	live_host = "127.0.0.1"
 
-    application.run(host = live_host, debug=True, extra_files=extra_files, threaded=True, port=8080)
+	application.run(host = live_host, debug=True, extra_files=extra_files, threaded=True)
--- modules/blueprints/login.py
+++ modules/blueprints/login.py
@@ -7,18 +7,18 @@ from modules.db import *
 from modules.decorators import *
 
 
-login_routes = Blueprint('login_routes', __name__, template_folder='templates')     #blueprint definition
+login_routes = Blueprint('login_routes', __name__, template_folder='templates')		#blueprint definition
 
 
 #this method creates a session for the store visitor before any request occurs
 @login_routes.before_request
 def setup_session():
-    sm = current_app.config['SessionManager']
-    s_id = current_app.config['session_cookie_id']
+	sm = current_app.config['SessionManager']
+	s_id = current_app.config['session_cookie_id']
 
-    if s_id not in session:
-        sm.open_session(current_app, session)
-        print "Created: ", session[s_id]
+	if s_id not in session:
+		sm.open_session(current_app, session)
+		print "Created: ", session[s_id]
 
 
 
@@ -27,12 +27,12 @@ def setup_session():
 @redirect_logged_in(current_app, session, 'cms_views.view_posts')
 @with_user_data(current_app, session)
 def login_redirect():
-    ctl = current_app.config["ctl"]
+	ctl = current_app.config["ctl"]
 
-    data = {}
-    data["ts"] = int(time.time())
+	data = {}
+	data["ts"] = int(time.time())
 
-    return render_template("login.html", data=data)
+	return render_template("login.html", data=data)
 
 
 
@@ -40,79 +40,79 @@ def login_redirect():
 @login_routes.route('/auth/login', methods=['POST'])
 @db_required
 def auth_login(db_wrapper):
-    login_info = request.form['login_info']
-    login_info = json.loads(login_info)
-    
-    user_data = db_wrapper.auth_user(login_info)
-    
-    if user_data:
-        sm = current_app.config['SessionManager']
-        set_session_data(sm, current_app, session, user_data);
-        return json.dumps(user_data)
-    else:
-        return json.dumps("invalid")
+	login_info = request.form['login_info']
+	login_info = json.loads(login_info)
+	
+	user_data = db_wrapper.auth_user(login_info)
+	
+	if user_data:
+		sm = current_app.config['SessionManager']
+		set_session_data(sm, current_app, session, user_data);
+		return json.dumps(user_data)
+	else:
+		return json.dumps("invalid")
 
 
 
 @login_routes.route('/logout')
 @db_required
 def logout(db_wrapper):
-    sm = current_app.config['SessionManager']
-    destroy_session_data(sm, current_app, session);
+	sm = current_app.config['SessionManager']
+	destroy_session_data(sm, current_app, session);
 
-    return redirect(url_for('blog_views.index'))
-    
+	return redirect(url_for('blog_views.index'))
+	
 
 
 def destroy_session_data(session_manager, app, session):
-    sessionPrefixList = ["auth:", ]
-    s_id = current_app.config['session_cookie_id']
+	sessionPrefixList = ["auth:", ]
+	s_id = current_app.config['session_cookie_id']
 
-    data = {}
-    data['key'] = session[s_id]
-    
-    val = session_manager.get_session_key(app, session, data)
+	data = {}
+	data['key'] = session[s_id]
+	
+	val = session_manager.get_session_key(app, session, data)
 
-    if val and val != "guest":
-        #delete all session data associated with this user from the redis table
-        for prefix in sessionPrefixList:
-            data["table"] = prefix + session[s_id]
-            result = session_manager.delete_session_hashTable(app, session, data)
+	if val and val != "guest":
+		#delete all session data associated with this user from the redis table
+		for prefix in sessionPrefixList:
+			data["table"] = prefix + session[s_id]
+			result = session_manager.delete_session_hashTable(app, session, data)
 
-        #preserve distinct login to a session
-        data["key"] = session[s_id]
-        data["value"] = "guest"
+		#preserve distinct login to a session
+		data["key"] = session[s_id]
+		data["value"] = "guest"
 
-        session_manager.update_session_key(app, session, data)
+		session_manager.update_session_key(app, session, data)
 
 
 
 
 
 def set_session_data(session_manager, app, session, user_data):
-    s_id = current_app.config['session_cookie_id']
+	s_id = current_app.config['session_cookie_id']
 
-    data = {}
-    data['key'] = session[s_id]
-    
-    val = session_manager.get_session_key(app, session, data)
+	data = {}
+	data['key'] = session[s_id]
+	
+	val = session_manager.get_session_key(app, session, data)
 
-    if val:
-        prefix = "auth:"
-        data["table"] = prefix + session[s_id]
+	if val:
+		prefix = "auth:"
+		data["table"] = prefix + session[s_id]
 
-        for key, value in user_data.iteritems():
-            if type(value) is type(None):
-                continue
+		for key, value in user_data.iteritems():
+			if type(value) is type(None):
+				continue
 
-            data["key"] = key
-            data["value"] = value
-            
-            session_manager.set_session_hashKey(app, session, data)
+			data["key"] = key
+			data["value"] = value
+			
+			session_manager.set_session_hashKey(app, session, data)
 
 
-        #preserve distinct login to a session
-        data["key"] = session[s_id]
-        data["value"] = user_data["username"]
-        
-        session_manager.update_session_key(app, session, data)
+		#preserve distinct login to a session
+		data["key"] = session[s_id]
+		data["value"] = user_data["username"]
+		
+		session_manager.update_session_key(app, session, data)
